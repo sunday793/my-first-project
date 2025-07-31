323a2a3f90da54e0a39b4ce6de93fdb72db2f97e
Sofia Sineglazova <sineglazova.sofiyska@gmail.com>
2025-07-31 21:02:27 +0300

    Added function to check amout of money

--- shop.py
+++ shop.py
@@ -2,8 +2,20 @@
 
 def sum_prod(prod: list[int]) -> int:
     s = sum(prod)
-    return f"This is the sum of all products: {s}."
+    return s
 
 prices = input("Enter the prices of the products: ").split()
 prices = list(map(int, prices))
-print(sum_prod(prices))
+s_p = sum_prod(prices)
+print(f"This is the total price: {s_p}.")
+
+def enough_money(s_p):
+    i = str(input(f"Do you want to pay {s_p}? y/n "))
+    if i == 'y':
+        return f"Great! You paid {s_p}."
+    elif i == 'n':
+        return "Come back later!"
+    else:
+        return "Error. Invalid input!"
+    
+print(enough_money(s_p))
